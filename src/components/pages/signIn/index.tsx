ecfb44124f06c8194c9b6ac70517e3659c169c35
Kyungbin <pkb8839@naver.com>
2022-09-15 13:10:55 +0900

    [Feat]: add signIn UI (react-hook-form을 이용하여, 에러 form처리를 진행

--- src/components/pages/signIn/index.tsx
+++ src/components/pages/signIn/index.tsx
@@ -1,5 +1,45 @@
+import { useForm } from "react-hook-form";
+
+import { Container, LoginForm } from "./styles";
+import Input from "@organisms/loginInput";
+import { INPUT_DATA } from "@constants/index";
+
+interface SignInFormInterface {
+  id: string;
+  password: string;
+}
+
 const SignIn = () => {
-    return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormInterface>({});
+
+  const onSubmit = (dd: SignInFormInterface) => {
+    console.log(dd);
   };
-  
-  export default SignIn;    
+
+  const inputProps = (type: "id" | "password") => {
+    return {
+      ...register(type, { required: type }),
+      ...INPUT_DATA[type],
+      ...{ error: errors[type]?.message },
+    };
+  };
+
+  return (
+    <Container direction="column">
+      <h2>로그인</h2>
+      <LoginForm onSubmit={handleSubmit(onSubmit)}>
+        <Input {...inputProps("id")} />
+        <Input {...inputProps("password")} />
+        <button>버튼</button>
+      </LoginForm>
+      <section>
+        <div>소셜 로그인</div>
+      </section>
+    </Container>
+  );
+};
+export default SignIn;
---
+++ src/components/pages/signIn/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { FlexDiv } from "@src/styles";
+
+const Container = styled(FlexDiv)`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginForm = styled.form``;
+
+export { Container, LoginForm };
